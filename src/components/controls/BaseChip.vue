66c036f70a81ecbf72cfff5f7960729950cb6f34
ImSirTea <45457205+ImSirTea@users.noreply.github.com>
2023-03-01 23:56:48 +0000

    Use more flexible styling

--- src/components/controls/BaseChip.vue
+++ src/components/controls/BaseChip.vue
@@ -10,7 +10,7 @@ withDefaults(defineProps<BaseChipProps>(), {square: false});
 </script>
 
 <template>
-  <div :class="{'border-solid': true, border: true, 'px-3': true, 'inline-block': true, 'rounded-full': !square, 'color': colour, flex: true, 'gap-2': true, 'items-center': true, 'py-0.5': true}">
+  <div :class="{'border-solid': true, border: true, 'px-3': true, 'inline-flex': true, 'rounded-full': !square, 'color': colour, flex: true, 'gap-2': true, 'items-center': true, 'py-0.5': true}">
     <slot />
   </div>
 </template>
