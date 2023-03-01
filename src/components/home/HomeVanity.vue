6ccebc372a2c14f945e88c9ebc6e055c5b0bd87c
ImSirTea <45457205+ImSirTea@users.noreply.github.com>
2023-03-01 00:56:50 +0000

    Re-flow of vanity

--- src/components/home/HomeVanity.vue
+++ src/components/home/HomeVanity.vue
@@ -1,17 +1,11 @@
 <script lang="ts" setup>
 import HomeVanityChips from "./HomeVanityChips.vue";
-
-
-
 </script>
 
 <template>
-  <section class="grid grid-cols-2 h-full">
-    <div />
-    <div class="my-auto">
-      <h1 class="text-6xl">Adam Lansley.</h1>
-      <h2 class="text-2xl">Front End Developer</h2>
-      <HomeVanityChips class="mt-2" />
-    </div>
+  <section class="flex flex-col items-center justify-center h-full text-center gap-y-2">
+    <h1 class="text-6xl inline-block">Adam Lansley</h1>
+    <h2 class="text-2xl inline-block secondary-text">Front End Developer</h2>
+    <HomeVanityChips class="mt-2" />
   </section>
 </template>
