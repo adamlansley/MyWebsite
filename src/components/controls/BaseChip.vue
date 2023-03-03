d9abde6e2777a5da801d707e033f515020d26600
ImSirTea <45457205+ImSirTea@users.noreply.github.com>
2023-03-03 19:25:08 +0000

    Chip fixes

--- src/components/controls/BaseChip.vue
+++ src/components/controls/BaseChip.vue
@@ -10,7 +10,10 @@ withDefaults(defineProps<BaseChipProps>(), {square: false});
 </script>
 
 <template>
-  <div :class="{'border-solid': true, border: true, 'px-3': true, 'inline-flex': true, 'rounded-full': !square, 'color': colour, flex: true, 'gap-2': true, 'items-center': true, 'py-0.5': true}">
+  <div :class="{border: true,  'border-zinc-600': true, 'px-3': true, 'inline-flex': true, 'rounded-full': !square, 'color': colour, flex: true, 'gap-2': true, 'items-center': true, 'py-0.5': true}">
     <slot />
   </div>
 </template>
+
+<style scoped>
+</style>
