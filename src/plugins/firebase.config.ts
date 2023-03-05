0fd22920d216c898ed6dcfc578ce98b58062881b
ImSirTea <45457205+ImSirTea@users.noreply.github.com>
2023-03-05 22:40:32 +0000

    Add config and cache

---
+++ src/plugins/firebase.config.ts
@@ -0,0 +1,10 @@
+export default {
+  apiKey: "AIzaSyAR-6MP8RXPjwOV8ctzzsxY2eob7m7r0FE",
+  authDomain: "mywebsite-633c6.firebaseapp.com",
+  projectId: "mywebsite-633c6",
+  storageBucket: "mywebsite-633c6.appspot.com",
+  messagingSenderId: "425569380586",
+  appId: "1:425569380586:web:90f3cdc135a723834a0f93",
+  measurementId: "G-TQ55NN3LTH"
+};
+
